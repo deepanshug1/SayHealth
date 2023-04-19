d352f97f52f91526e65dcd3f8a1df513f8dd23f6
deepanshug1 <95443111+deepanshug1@users.noreply.github.com>
2023-04-19 18:18:59 +0530

    final commit

---
+++ app/src/main/java/DTI/SH/sayhealth/payment.java
@@ -0,0 +1,17 @@
+package DTI.SH.sayhealth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+    }
+}
